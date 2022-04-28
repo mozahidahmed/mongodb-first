63a6dd337cc9d41715cf51563c4e9bd26f18a114
mozahidsani <mozahidwebmin11d10@gmail.com>
2022-04-28 12:57:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app=express();
+
+const port=process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+//......................................................................
+
+     
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://carservice:ovW3c21PM4LPKCBy@cluster0.xcemb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+
+  try{
+    await client.connect();
+    const userCollection=client.db('car-service').collection('service');
+    
+  
+          app.get('/service',async (req,res)=>{
+            const query={};
+          const cursor=userCollection.find(query);
+          const service=await cursor.toArray();
+          res.send(service);
+          });
+  
+  
+  
+  
+  
+  
+  }
+  finally{
+  
+  }
+}
+run().catch(console.dir);
+                
+//......................................................................
+
+
+app.get('/',(req,res)=>{
+    res.send('runnung car servicing MOZAHID MY NAME');
+})
+app.listen(port,()=>{
+    console.log('listing to port MY name is Mozahid',port)
+})
